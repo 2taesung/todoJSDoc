a71e22b36261d5535dca933b9f542f4f28360afe
dlxotjde <dlxotjde@daum.net>
2023-02-13 02:20:34 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+/**
+ *@todo  required: 아이디, 내용, 완료여부
+ *@todo  optional: 태그들
+*/
+const todo = {}
+
+/**
+ * @createTodo 할 일을 추가할 수 있다.
+ * @createTodo 내용없이 추가할 수 없다.
+ */
+function createTodo() {}
+
+/**
+ * @readTodo 모든 할 일을 조회할 수 있다.
+ * @readTodo ID를 기반으로 특정 할 일을 조회할 수 있다.
+ */
+function readTodo() {}
+
+/**
+ * @updateTodo ID를 제외한 모든 속성을 수정할 수 있다.
+ * @updateTodo 특정 할 일의 특정 태그를 수정할 수 있다.
+ */
+function updateTodo() {}
+
+/**
+ * @deleteTodo ID를 기반으로 특정 할 일을 삭제할 수 있다.
+ * @deleteTodo 모든 할 일을 제거할 수 있다.
+ * @deleteTodo 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * @deleteTodo 특정 할 일의 모든 태그를 제거할 수 있다.
+ */
+function deleteTodo() {}
